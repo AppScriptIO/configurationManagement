e2f231d875d43456f727bd541741c3f73f83c1cf
Dr. Safi <sfmissive@gmail.com>
2019-01-24 20:41:36 +0200

    🐛 Validate configuration data for `rootPath` option.

--- source/Configuration.class.js
+++ source/Configuration.class.js
@@ -1,3 +1,5 @@
+const assert = require('assert')
+
 /** 
  * An interface for reading configuration object in a way that follows a configuration specification (`appscript` projects configuration rules). 
  * TODO: integrate a plugin system to allow extending the configuration api to follow different rules or specifications.
@@ -17,6 +19,8 @@ export class Configuration {
     }
 
     get rootPath() {
+        let rootPath = this.configuration.directory.application.rootPath
+        assert(rootPath, `❌ Configuration 'rootPath' option must be defined.`) // validate
         return this.configuration.directory.application.rootPath
     }
 }
