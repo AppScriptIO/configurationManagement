e3d576ab4eeab11fe7fe097794c839e57f96e6ab
Safi <sfmissive@gmail.com>
2019-05-06 02:13:10 +0300

    ⚗ Move .gitignore as issue with Windows symbolic links appeared.

--- script/build.sh
+++ script/build.sh
@@ -1,8 +1,8 @@
-rm -r ./distribution
+rm -r ./distribution && \
 # entrypoint
-mkdir -p ./distribution/entrypoint/programmaticAPI/
-echo "module.exports = require('../../source/script.js')" >> ./distribution/entrypoint/programmaticAPI/index.js
+mkdir -p ./distribution/entrypoint/programmaticAPI/ && \
+echo "module.exports = require('../../source/script.js')" >> ./distribution/entrypoint/programmaticAPI/index.js && \
 # source
-babel --out-dir ./distribution/source "./source/*" --config-file "./configuration/babel.config.js"
+babel --out-dir ./distribution/source "./source/*" --config-file "./configuration/babel.config.js" && \
 # package.json
 babel --out-dir ./distribution/ "./package.json" --config-file "./configuration/babel.config.js" --copy-files
--- script/distribute.sh
+++ script/distribute.sh
@@ -1,18 +1,20 @@
 # https://help.github.com/articles/splitting-a-subfolder-out-into-a-new-repository/
 # This will create a new branch with one commit that adds everything in HEAD. It doesn't alter anything else, so it's completely safe. 
 # Used to create a release from a repo where the release will contain a single commit.
-git branch distribution $(echo "Release 1.0.0" | git commit-tree HEAD^{tree}) && git checkout distribution \
-&& git filter-branch \
+git branch distribution $(echo "Release 0.9.0" | git commit-tree HEAD^{tree}) && \ 
+git checkout distribution && \
+echo "Release 0.9.0" | git commit-tree HEAD^{tree} && \
+git filter-branch \
     --tree-filter 'find . ! \( -path "./configuration*" -o \
-                               -path "./package.json" -o \
-                               -path "./README.md" -o \
-                               -path "./yarn.lock" -o \
-                               -path "./source" -o \
-                               -path "." \) \
+                                -path "./package.json" -o \
+                                -path "./README.md" -o \
+                                -path "./yarn.lock" -o \
+                                -path "./source" -o \
+                                -path "." \) \
                         -exec rm -fr {} +' \
     --prune-empty -f \
     distribution && \
-git tag -a 1.0.0 -m "Realse tag 1.0.0" && \
-git push origin 1.0.0 && \
+git tag -a 0.9.0 -m "Realse tag 0.9.0" && \
+git push origin 0.9.0 && \
 git checkout master && \
-git branch -D distribution
+git branch -D distribution \
