ba05d74782779d1299aa464671d4ba16bcc9935d
Safi <nassarsafi@gmail.com>
2020-01-23 03:54:22 +0200

    📦 Bump package.json version.

--- entrypoint/programmaticAPI/index.js
+++ entrypoint/programmaticAPI/index.js
@@ -6,7 +6,7 @@ const projectConfig = require('../../configuration'),
 if (filesystem.existsSync(projectConfig.directory.distribution)) module.exports = require(projectConfig.directory.distribution)
 else {
   // • Transpilation (babelJSCompiler)
-  const { Compiler } = require('@dependency/javascriptTranspilation')
+  const { Compiler } = require('@deployment/javascriptTranspilation')
   let compiler = new Compiler({ callerPath: __dirname })
   compiler.requireHook()
   module.exports = require(path.join(projectConfig.directory.source, projectConfig.entrypoint.programmaticAPI))
